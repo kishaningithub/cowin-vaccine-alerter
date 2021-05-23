1f009100c331dc0e53a66bf0c4175b33d5127e4d
Kishan B <kishancs46@gmail.com>
2021-05-23 22:57:47 +0530

    Add more districts in tamil nadu

--- README.md
+++ README.md
@@ -2,19 +2,13 @@
 This project is a telegram bot which alerts subscribers when vaccination slot is available.
 
 ## Supported areas
-- Chennai
-- More can be added based on interest
+See [homepage](https://kishaningithub.github.io/cowin-vaccine-alerter/) to view supported regions more can 
+always be added based on interest
 
 ## Background
 When I tried to book normally or with the help of other telegram bots that are out there, there was a considerable lag between the time
 when the slot is available and when the notification is received. This is primarily due to the limitations imposed by the Cowin public API
 Reference - https://apisetu.gov.in/public/marketplace/api/cowin/cowin-public-v2
-
-## Features
-- [x] Alerts for 18+ for Chennai, Tamilnadu - https://t.me/joinchat/sew-FrT6W9U0NDhl
-
-## Look ahead
-The expansion will be based on the interest this project generates over time.
 
 # Development
 * Install [direnv](https://direnv.net/)
--- cowin-vaccine-alerter.sh
+++ cowin-vaccine-alerter.sh
@@ -3,13 +3,15 @@
 set -e
 
 poll_cowin_and_send_alert() {
+  district_id="$1"
+  chat_id="$2"
   rm -f ./*.json
   rm -f ./*.txt
   current_date=$(TZ=Asia/Kolkata date +"%d-%m-%Y")
-  curl --retry 3 -fL "${COWIN_API_BASE_URL}/v2/appointment/sessions/calendarByDistrict?district_id=571&date=${current_date}" -H "${USER_AGENT_HEADER}" -H 'Accept: application/json, text/plain, */*' -H 'Accept-Language: en,en-US;q=0.5' --compressed -H "${COWIN_AUTHORIZATION_TOKEN}" -H "Origin: ${ORGIN_URL}" -H 'Connection: keep-alive' -H "Referer: ${ORGIN_URL}" -H 'Sec-GPC: 1' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'TE: Trailers' | jq > ./cowin-chennai.json
+  curl --retry 3 -fL "${COWIN_API_BASE_URL}/v2/appointment/sessions/calendarByDistrict?district_id=${district_id}&date=${current_date}" -H "${USER_AGENT_HEADER}" -H 'Accept: application/json, text/plain, */*' -H 'Accept-Language: en,en-US;q=0.5' --compressed -H "${COWIN_AUTHORIZATION_TOKEN}" -H "Origin: ${ORGIN_URL}" -H 'Connection: keep-alive' -H "Referer: ${ORGIN_URL}" -H 'Sec-GPC: 1' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'TE: Trailers' | jq > ./cowin-chennai.json
   jq -r '[.centers[] | select(.sessions[] | .min_age_limit == 18 and .available_capacity_dose1 > 0)]' ./cowin-chennai.json | jq -r '.[] | "Dose 1 of \([.sessions[].vaccine] | unique | join(",")) available at \(.name), \(.block_name), \(.pincode)"' | uniq > dose-1-result.txt
   #  jq -r '[.centers[] | select(.sessions[] | .min_age_limit == 18 and .available_capacity_dose2 > 0)]' ./cowin-chennai.json | jq -r '.[] | "Dose 2 of \([.sessions[].vaccine] | unique | join(",")) available at \(.name), \(.block_name), \(.pincode)"' | uniq > dose-2-result.txt
-  cat dose-1-result.txt | xargs -I {} curl --retry 3 -fLG "https://api.telegram.org/${TELEGRAM_BOT_TOKEN}/sendMessage" --data-urlencode "chat_id=${TELEGRAM_BOT_CHAT_ID}" --data-urlencode "text={}"
+  cat dose-1-result.txt | xargs -I {} curl --retry 3 -fLG "https://api.telegram.org/${TELEGRAM_BOT_TOKEN}/sendMessage" --data-urlencode "chat_id=${chat_id}" --data-urlencode "text={}"
 }
 
 max_attempts=500
@@ -17,7 +19,14 @@ sleep_time=30
 attempt_counter=0
 while [ ${attempt_counter} -le ${max_attempts} ]
 do
-  poll_cowin_and_send_alert
+  # Tamil Nadu / Chennai
+  poll_cowin_and_send_alert 571 -530040865
+  # Tamil Nadu / Tiruvallur
+  poll_cowin_and_send_alert 572 -516074907
+  # Tamil Nadu / Chengalpet
+  poll_cowin_and_send_alert 565 -540515683
+  # Tamil Nadu / Kanchipuram
+  poll_cowin_and_send_alert 557 -519869836
   sleep ${sleep_time}
   attempt_counter=$((attempt_counter+1))
 done
